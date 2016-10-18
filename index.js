0c0a7eddf070b7fb3335220cc592c922a09bbdff
Lais Andrade <lais.andrade@liferay.com>
2016-10-18 12:05:15 -0300

    Uses usual node port 3000.

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('Listening on port 80');
 });
